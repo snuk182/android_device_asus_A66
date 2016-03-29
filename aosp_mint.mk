e4f93645cbc1cde17d4634ace09b7346f2eadd95
Adrian DC <radian.dc@gmail.com>
2016-03-29 18:45:04 +0200

    build: Add Android AOSP build support
     * Add aosp_mint vendorsetup
     * Add aosp_mint.mk with BOARD_AOSP_BASED enabled
    
    Change-Id: I49ec15dddc775ce5d294b33e4846b7382176e402
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ aosp_mint.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2013-2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Device configurations for AOSP
+BOARD_AOSP_BASED := true
+
+# Inherit device configurations
+$(call inherit-product, device/sony/mint/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := mint
+PRODUCT_NAME := aosp_mint
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia T
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT30p BUILD_FINGERPRINT=Sony/LT30p/LT30p:4.3/9.2.A.1.205/Mvv_tg:user/release-keys PRIVATE_BUILD_DESC="LT30p-user 4.3 JB-MR2-BLUE-140313-0947 257 test-keys"
