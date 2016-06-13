73b728e92c0df60094ad7c3424ac8b907ab2f30e
AdrianDC <radian.dc@gmail.com>
2016-06-13 22:49:00 +0200

    mint: Cleanup the device tree sources
     * Restructure the device tree into fragments
     * Move common blue parameters to blue-common
     * Minimize the amount of duplicated code in devices
     * Unify the cm.mk into one clean .mk
     * Delete useless config flags
     * Cleanup a few double lines
    
    Change-Id: I65b3731eb11f6f160fea257b530151f7997bbd7b
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,12 +1,38 @@
-$(call inherit-product, device/sony/mint/full_mint.mk)
+#
+# Copyright (C) 2011-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit CM common Phone stuff.
+# Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT30p BUILD_FINGERPRINT=Sony/LT30p/LT30p:4.3/9.2.A.1.205/Mvv_tg:user/release-keys PRIVATE_BUILD_DESC="LT30p-user 4.3 JB-MR2-BLUE-140313-0947 257 test-keys"
+# Inherit device configurations
+$(call inherit-product, device/sony/mint/device.mk)
 
-PRODUCT_NAME := cm_mint
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
 PRODUCT_DEVICE := mint
+PRODUCT_NAME := cm_mint
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia T
+
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT30p BUILD_FINGERPRINT=Sony/LT30p/LT30p:4.3/9.2.A.1.205/Mvv_tg:user/release-keys PRIVATE_BUILD_DESC="LT30p-user 4.3 JB-MR2-BLUE-140313-0947 257 test-keys"
---
+++ product/hardware.mk
@@ -0,0 +1,4 @@
+# Hardware configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/hw_config.sh:system/etc/hw_config.sh \
+    $(LOCAL_PATH)/rootdir/etc/pre_hw_config.sh:system/etc/pre_hw_config.sh
